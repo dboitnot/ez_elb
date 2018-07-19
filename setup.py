c89b325c94f646e0f218f163cfc4ea2ad27204ad
Daniel Boitnott <dboitnot@gmail.com>
2018-07-19 10:26:04 -0500

    Added initial travis config

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ez_elb",
-    version="0.1.0",
+    version="0.1.5",
     author="Dan Boitnott",
     author_email="boitnott@sigcorp.com",
     description="Easily define complex ELB CloudFormation templates",
@@ -18,4 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
+    install_requires=[
+        'troposphere>=2.3.1'
+    ]
 )
