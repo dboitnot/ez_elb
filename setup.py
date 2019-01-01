022384e7f48019dd9c0b223055af06bf307fd068
Daniel Boitnott <dboitnot@gmail.com>
2019-01-01 14:08:45 -0600

    Fixed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ez_elb",
-    version="0.1.8",
+    version="0.2.1",
     author="Dan Boitnott",
     author_email="boitnott@sigcorp.com",
     description="Easily define complex ELB CloudFormation templates",
